addf428842c29c70f1716193bb10263dae98941f
rmp <ya>
2023-05-31 17:30:10 +0300

    roma

--- app/src/main/java/com/example/demoexam_prudovskiy/MainActivity3.java
+++ app/src/main/java/com/example/demoexam_prudovskiy/MainActivity3.java
@@ -8,19 +8,19 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
 
         LinearLayout linearLayout4 = (LinearLayout) findViewById(R.id.linearLayout4);
 
         linearLayout4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
                 startActivity(intent);
             }
         });
@@ -30,7 +30,7 @@ public class MainActivity2 extends AppCompatActivity {
         textView66.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity2.this, MainActivity5.class);
+                Intent intent = new Intent(MainActivity3.this, MainActivity5.class);
                 startActivity(intent);
             }
         });
