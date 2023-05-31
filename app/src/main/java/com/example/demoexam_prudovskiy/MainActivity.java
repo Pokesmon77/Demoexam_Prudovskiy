d00be7f251583f99fe7d061597b21dafec080c29
rmp <ya>
2023-05-31 16:56:29 +0300

    roma

--- app/src/main/java/com/example/demoexam_prudovskiy/MainActivity.java
+++ app/src/main/java/com/example/demoexam_prudovskiy/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.demoexam_prudovskiy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
+
 }
