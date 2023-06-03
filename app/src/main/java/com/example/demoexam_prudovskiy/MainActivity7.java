a4f69959a7b3d4323be809429b4ca5b3cb220100
Денис <Deniffka998@gmail.com>
2023-06-04 00:10:49 +0300

    roma

--- app/src/main/java/com/example/demoexam_prudovskiy/MainActivity7.java
+++ app/src/main/java/com/example/demoexam_prudovskiy/MainActivity7.java
@@ -3,6 +3,9 @@ package com.example.demoexam_prudovskiy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity7 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+
+        ImageView pin1 = findViewById(R.id.pin1);
+        Button a = findViewById(R.id.button17);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pin1.setImageResource(R.drawable.epsb);
+            }
+        });
     }
 }
